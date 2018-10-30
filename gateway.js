3eedbfbc60203f14ca5ea32c5bcdc950ab477076
Alexander Gorshunov <gorshunov.a.k@sberbank.ru>
2018-10-30 12:45:37 +0300

    Add plugin for Xiaomi Smart Monitoring

--- gateway.js
+++ gateway.js
@@ -1,26 +1,47 @@
 const miio = require('miio');
+const { power } = require('abstract-things/values');
+const { energy } = require('abstract-things/values');
+const moment = require('moment');
+const config = require('./config');
+const requestPromise = require('request-promise');
+const requestLogstash = requestPromise.defaults({
+  baseUrl: config.logstash.url,
+  json: true,
+});
 
 const main = async () => {
 
   try {
 
     const device = await miio.device({address: '192.168.2.170', token: '0bc1cbdf0556cac735574fc7102d5846'});
-    console.log('Connected to', device)
+    // console.log('Connected to', device)
     const children = device.children();
     for (const child of children) {
-      if(child.matches('type:power-plug') && child.matches('cap:power-consumed')) {
-        console.log('Power is', await child.power(), child);
-        const powerConsumed = await child.powerConsumed();
-        console.log('Power consumed:', powerConsumed);
-        child.on('powerConsumedChanged', v => console.log('Changed to:', v));
+      if (child.matches('type:power-plug') && child.matches('cap:power-load')) {
+        let id = child.id;
+        const powerLoad = await child.powerLoad();
+        // console.log('Plug Id is:', id);
+        // console.log('Power load:', powerLoad.watts);
+        // const powerConsumed = await child.powerConsumed();
+        // console.log('Power consumed:', powerConsumed);
+        await requestLogstash.post('/', {
+          body: {
+            '@timestamp': moment().utcOffset('+03:00').format('YYYY-MM-DDTHH:mm:ss.SSSZ'),
+            id: id,
+            powerload: powerLoad,
+            deviceType: 'xiaomiSmartPlug',
+          }
+        })
       }
     }
+    device.destroy()
+
+
 
   } catch (e) {
     console.error(e);
   }
 
-
 };
 
-main()
+setInterval(main, 2000);
--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+const miio = require('miio');
+const { power } = require('abstract-things/values');
+const { energy } = require('abstract-things/values');
 const moment = require('moment');
 const config = require('./config');
 const requestPromise = require('request-promise');
@@ -16,13 +19,51 @@ const getSonoffData = async() => {
     await requestLogstash.post('/', {
       body: {
         '@timestamp': moment().utcOffset('+03:00').format('YYYY-MM-DDTHH:mm:ss.SSSZ'),
-        energy: result.EnergyReset
+        energy: result.EnergyReset,
+        device_id: 'sonoff:device1',
+        device_type: 'sonoffPow',
+        user_id: config.user.id,
       }
     })
   } catch(error) {
     console.error(error);
   }
 };
+setInterval(getSonoffData, 2000);
 
-var timerId = setInterval(getSonoffData, 2000);
 
+const main = async () => {
+
+  try {
+
+    const device = await miio.device({address: '192.168.2.170', token: '0bc1cbdf0556cac735574fc7102d5846'});
+    // console.log('Connected to', device)
+    const children = device.children();
+    for (const child of children) {
+      if (child.matches('type:power-plug') && child.matches('cap:power-load')) {
+        let id = child.id;
+        const powerLoad = await child.powerLoad();
+        // console.log('Plug Id is:', id);
+        // console.log('Power load:', powerLoad.watts);
+        await requestLogstash.post('/', {
+          body: {
+            '@timestamp': moment().utcOffset('+03:00').format('YYYY-MM-DDTHH:mm:ss.SSSZ'),
+            powerload: powerLoad,
+            device_id: id,
+            device_type: 'xiaomiSmartPlug',
+            user_id: config.user.id,
+          }
+        })
+      }
+    }
+    device.destroy()
+
+
+
+  } catch (e) {
+    console.error(e);
+  }
+
+};
+
+setInterval(main, 2000);
